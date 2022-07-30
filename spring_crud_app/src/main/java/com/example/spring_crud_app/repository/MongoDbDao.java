847572a413699fe0407dd214721615fc47a62b4a
SourabhH <SourabhH@sourabhh.emids.local>
2022-07-30 14:18:52 +0530

    added new class for mongodb connection

---
+++ spring_crud_app/src/main/java/com/example/spring_crud_app/repository/MongoDbDao.java
@@ -0,0 +1,30 @@
+package com.example.spring_crud_app.repository;
+
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class MongoDbDao {
+
+	public static void main(String[] args) {
+
+		// System.setProperty("jdk.tls.trustNameService", "true");
+
+		MongoClientURI uri = new MongoClientURI(
+				"mongodb+srv://sourabhhardeniya:Sourabh%401@cluster0.7sjud4i.mongodb.net/?retryWrites=true&w=majority&connectTimeoutMS=30000");
+
+		try (MongoClient mongoClient = new MongoClient(uri)) {
+			MongoDatabase database = mongoClient.getDatabase("test");
+			MongoCollection<Document> collection = database.getCollection("test collection");
+			Document query = new Document("_id", new ObjectId("62e4347cb00638299bbc8ff7"));
+			Document result = collection.find(query).iterator().next();
+
+			System.out.println("name: " + result.getString("name"));
+			System.out.println("age: " + result.getString("age"));
+		}
+	}
+}
